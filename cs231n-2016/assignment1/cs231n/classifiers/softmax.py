c8177f7512e42875a47f7e7a1db500acf8eee4a4
xnzhang <xnzhang@nttpc.co.jp>
2018-11-27 10:25:53 +0900

    no message

---
+++ cs231n-2016/assignment1/cs231n/classifiers/softmax.py
@@ -0,0 +1,100 @@
+import numpy as np
+from random import shuffle
+
+def softmax_loss_naive(W, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_class = W.shape[1]
+  num_train = X.shape[0]
+  dW_i = np.zeros_like(dW)
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    scores -= np.max(scores)                   # scores with normalization logC
+    score_yi = scores[y[i]]
+    margin = 0
+    margin -= score_yi
+    sum_exp_score = 0
+    for j in xrange(num_class):
+      if j == y[i]:
+        sum_exp_score += np.e**score_yi
+        continue
+      exp_score = np.e**scores[j]
+      dW_i[:, j] = exp_score*X[i]
+      sum_exp_score += exp_score
+    margin += np.log(sum_exp_score)
+    loss += margin
+    dW_i[:, y[i]] = -(sum_exp_score-np.e**score_yi)*X[i]
+    # divide by the sum of exp for current data point all at once
+    dW_i /= sum_exp_score
+    dW += dW_i
+  loss = (loss/num_train) + 0.5*reg*np.sum(W*W)
+  dW = (dW/num_train) + reg*W
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_class = W.shape[1]
+  num_train = X.shape[0]
+  Score = X.dot(W)
+  Score = Score - Score.max(axis=1, keepdims=True)
+  Exp_score = np.exp(Score)
+  Probability = Exp_score / Exp_score.dot(np.ones([num_class, 1]))
+  Margin = np.log(Probability)
+  Mask_y = np.zeros_like(Score)
+  Mask_y[xrange(num_train), y] = 1.
+  Mask_Margin = Mask_y * Margin
+  loss = -np.sum(Mask_Margin) / num_train + 0.5*reg*np.sum(W*W)
+  
+  dProbability = -Probability
+  dProbability[xrange(num_train), y] += 1
+  dW = -X.T.dot(dProbability) / num_train + reg*W
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
